a8df59909b4f24a643f8bf36d403e16cef70569f
Kateryna Anisimova <110602507+Kate2610@users.noreply.github.com>
2023-08-30 11:30:34 +0300

    Update 1.ts

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -5,6 +5,8 @@ let name: string = 'Max';
 let toggle: boolean = true;
 let empty: null = null;
 let notInitialize: unknown;
-let callback = (a: number) => (b: number): number => { return 100 + b };
+let callback = (a: number): number => {
+  return 100 + a;
+}; 
 
 export {};
